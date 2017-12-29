03a8b5976a11a98bc5b109e19ab737415d434589
Session-gao <gaopeiliang@vispractice.com>
2017-12-29 17:09:06 +0800

    Initial commit

---
+++ src/test/java/io/cloudins/springbootauthenticationcenter/SpringbootAuthenticationCenterApplicationTests.java
@@ -0,0 +1,16 @@
+package io.cloudins.springbootauthenticationcenter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootAuthenticationCenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
